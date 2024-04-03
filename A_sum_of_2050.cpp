f2bf0bfd0fc18c78a1768ad75830dd586ac377f0
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-03 11:22:57 +0600

    Wed Apr  3 11:22:57 AM +06 2024

---
+++ A_sum_of_2050.cpp
@@ -0,0 +1,25 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main() {
+  long long t;
+  cin >> t;
+  while (t--) {
+    long long n;
+    cin >> n;
+    int ans = 0;
+    while (n >= 2050) {
+      ans++;
+      long long x = 2050;
+      while (x <= n) {
+        x *= 10;
+      }
+      x /= 10;
+      n = n - x;
+    }
+    if (n != 0)
+      cout << -1 << endl;
+    else
+      cout << ans << endl;
+  }
+  return 0;
+}
