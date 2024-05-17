da348a45f640bd69549ca1b3e5844cdd693d5ab3
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-05-17 02:23:32 +0000

    Fri May 17 02:23:32 AM UTC 2024

--- Contest/B-Compare-T-Shirt-Sizes.cpp
+++ Contest/B-Compare-T-Shirt-Sizes.cpp
@@ -4,5 +4,39 @@ int main() {
   int t;
   cin >> t;
   while (t--) {
+    string s1, s2;
+    cin >> s1 >> s2;
+    if (s1 == s2) {
+      cout << "=" << endl;
+    } else if (s1[s1.length() - 1] == 'S' and s2[s2.length() - 1] == 'S') {
+      if (s1.length() > s2.length()) {
+        cout << "<" << endl;
+      } else {
+        cout << ">" << endl;
+      }
+    } else if (s1[s1.length() - 1] == 'L' or s2[s2.length() - 1] == 'L') {
+      if (s1.length() > s2.length()) {
+        cout << ">" << endl;
+      } else {
+        cout << "<" << endl;
+      }
+    } else if (s1[s1.length() - 1] == 'M' and s2[s2.length() - 1] == 'S') {
+      cout << ">" << endl;
+    } else if (s1[s1.length() - 1] == 'S' and s2[s2.length() - 1] == 'M') {
+      cout << "<" << endl;
+    } else if (s1 == "M" and s2 == "L") {
+      cout << "<" << endl;
+    } else if (s1 == "L" and s2 == "M") {
+      cout << ">" << endl;
+    } else if (s1 == "M" and s2[s2.length() - 1] == 'L') {
+      cout << "<" << endl;
+    } else if (s1[s1.length() - 1] == 'L' and s2 == "M") {
+      cout << ">" << endl;
+    } else if (s1[s1.length() - 1] == 'S' and s2[s2.length() - 1] == 'L') {
+      cout << "<" << endl;
+    } else if (s1[s1.length() - 1] == 'L' and s2[s2.length() - 1] == 'S') {
+      cout << ">" << endl;
+    }
   }
+  return 0;
 }
--- Contest/C-Clock_Strings.cpp
+++ Contest/C-Clock_Strings.cpp
@@ -1,33 +1,19 @@
 #include <bits/stdc++.h>
 
 #define ll long long
-
 using namespace std;
-
 int main() {
-
   ios::sync_with_stdio(false);
-
   cin.tie(NULL);
-
   ll tc;
-
   cin >> tc;
-
   while (tc--) {
-
     ll a, b, c, d;
-
     cin >> a >> b >> c >> d;
-
     bool flag1 = false, flag2 = false;
-
     ll tmp = max(a, b);
-
     a = min(a, b);
-
     b = tmp;
-
     for (int i = a; i <= b; i++)
 
     {
